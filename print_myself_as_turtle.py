604b43f10a9c8f85ebca65ff4a0d5288fd3edfba
Oleksandr Panov <oleksandr.panov@styrax.cz>
2022-10-16 17:30:45 +0200

    upd: print pixel as stamp

--- print_myself_as_turtle.py
+++ print_myself_as_turtle.py
@@ -113,7 +113,8 @@ letters = {
     ],
 }
 
-pixel_size = 3 # Then all measurements in 'virtual' pixels
+REAL_PEN_SIZE = 20 # Found in google
+pixel_size = 4 # Then all measurements in 'virtual' pixels
 spaces_in_tab = 4
 margin_horizontal = 0
 margin_vertical = 1
@@ -121,8 +122,8 @@ letter_height = len(letters['a'])
 letter_width = len(letters['a'][0])
 char_1 = '#'
 char_0 = ' '
-padding_vertical = 1
-padding_horizontal = 1
+padding_vertical = 3
+padding_horizontal = 3
 # turtle_speed = 10 # 1..10
 turtle_speed = 'fastest'
 hide_turtle = False
@@ -130,8 +131,8 @@ hide_turtle = False
 
 
 file_path = os.path.abspath(__file__)
-file = open('.gitignore', 'r', encoding='utf-8')
-# file = open(file_path, 'r', encoding='utf-8')
+# file = open('.gitignore', 'r', encoding='utf-8')
+file = open(file_path, 'r', encoding='utf-8')
 file_contents = file.read().replace('\r', '')
 lines = file_contents.split('\n')
 
@@ -146,6 +147,8 @@ turtle.getscreen().screensize(
     pixel_size * screen_width, 
     pixel_size * screen_height
 )
+turtle.shape('square')
+turtle.shapesize(pixel_size / REAL_PEN_SIZE, pixel_size / REAL_PEN_SIZE, 0)
 turtle.pencolor(0, 0, 0)
 turtle.fillcolor(0, 0, 0)
 turtle.speed(turtle_speed)
@@ -157,21 +160,15 @@ ts.yview_moveto(pixel_size * screen_height / -2)
 turtle.penup()
 
 def draw_pixel():
-    turtle.pendown()
-    turtle.begin_fill()
-    for i in range(4):
-        turtle.forward(pixel_size)
-        turtle.right(90)
-    turtle.end_fill()
-    turtle.penup()
+    turtle.stamp()
 
 def goto_line(line_index, pseudo_index_inside_line):
     real_x = pixel_size * (padding_horizontal)
     real_y = pixel_size * (padding_vertical + line_index * (letter_height + margin_vertical) + pseudo_index_inside_line)
 
     # Because 0,0 is in the center
-    real_x = real_x - screen_width*pixel_size/2
-    real_y = screen_height*pixel_size/2 - real_y
+    real_x = real_x - (screen_width*pixel_size/2 + pixel_size / 2)
+    real_y = (screen_height*pixel_size/2 - pixel_size / 2) - real_y
 
     # turtle.setpos(real_x, real_y)
     turtle.setposition(real_x, real_y)
